b35a654d0e04d4d6009c6a26d9e93f6f1f797371
Nikkaushal007 <nikhilkaushal613@gmail.com>
2022-12-27 15:56:36 +0530

    first commit

---
+++ Signupform.js
@@ -0,0 +1,11 @@
+function callvalue(){
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var number=document.getElementById("number").value;
+    var timeforcall=document.getElementById("timeforcall").value;
+    document.writeln("Your info: "+"<br>"+"your name: "+name +"<br>");
+    document.writeln("Your email: "+email);
+    document.writeln("Your number: "+number);
+    document.writeln("timeforcall: "+timeforcall);
+
+}
